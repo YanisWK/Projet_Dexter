9ae60b4af9dea6c0bca35a7272f5dbaec56d2667
Victor Roque Gomes <victorloic22@gmail.com>
2024-01-22 11:37:40 +0100

    Ajout d'un paramètres temps et de la fonctions avancer dans la class

--- class/Robot.py
+++ class/Robot.py
@@ -1,6 +1,10 @@
 class Robot:
     vitesses
     position
+    temps
     def __init__(self,vitesses,position) -> None:
         self.vitesses = vitesses
         self.position = position
+    
+    def avancer(self,temps):
+        self.position = self.position + self.vitesses*temps
