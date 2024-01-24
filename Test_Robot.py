a00c7b8d3d95a6f562602129c4d9b22894e1ae80
LuckeMao <maolucke@gmail.com>
2024-01-24 09:02:33 +0100

    Ajout des textes pour plus de clareté dans les test

--- Test_Robot.py
+++ Test_Robot.py
@@ -2,17 +2,28 @@ from Robot import *
 from math import *
 
 rb = Robot(12)
+print("Affichage du robot:")
 print(rb)
+print("\n")
+
+print("Le robot avance de 10cm:")
 rb.avancer(10)
 print(rb)
+print("\n")
+
+print("Le robot tourne de 90° vers la droite et avance de 5cm:")
 rb.tourner_a_droite(90)
 rb.avancer(5)
 print(rb)
+print("\n")
+
+print("Le robot fait un demi-tour et avance de 5cm:")
 rb.tourner_a_gauche(180)
-print(rb)
 rb.avancer(5)
 print(rb)
+print("\n")
+
+print("Le robot tourne à droite de 90° vers la droite et recule de 10cm:")
 rb.tourner_a_droite(90)
-print(rb)
 rb.reculer(10)
 print(rb)
