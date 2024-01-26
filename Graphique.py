fdbcd03502d1a95947b809ff69d7e463a3914575
SniKoxx <huang.loic2004@gmail.com>
2024-01-26 14:38:38 +0100

    Préparation de l'environnement pour l'interface

--- Graphique.py
+++ Graphique.py
@@ -1,6 +1,17 @@
 from time import *
 from tkinter import *
 
-
+#Spécificité de notre interface graphique
 window = Tk()
+longueur = 1080
+largeur = 720
+window.title("Simulation")
+window.geometry(f"{longueur+200}x{largeur+5}")
+window.minsize(480,360)
+window.maxsize(1920,1080)
+
+window.resizable(height=True, width=True)
+
+rec_base = Canvas(window, bg='blue', width=longueur, height=largeur)
+rec_base.place(x='0',y='0')
 window.mainloop()
