36207451bba6c2f8f0f5de7c766bc22ac60b8eae
LuckeMao <maolucke@gmail.com>
2024-01-26 15:38:55 +0100

    Ajout des attributs dans Simulation

--- Source/Simulation.py
+++ Source/Simulation.py
@@ -1,3 +1,8 @@
 class Simulation:
-    def __init__(self, id):
+    def __init__(self, id, robot, environnement, temps):
         self.id = id
+        self.robot = robot                  #Le robot
+        self.environnement = environnement  #L'environnement
+        self.temps = temps                  #Le nombre de rafraichissement par seconde
+        self.vitesse = 100                  #La vitesse à laquelle le robot se déplace
+        self.distance = 100                 #La distance que le robot va parcourir
