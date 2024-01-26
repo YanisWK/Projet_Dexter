d6d5c0f697a75a1f8b0895ae99941eac81a07de4
vntgogh <ines.ediane29@gmail.com>
2024-01-26 16:33:18 +0100

    maj commentaire

--- Source/Robot.py
+++ Source/Robot.py
@@ -12,17 +12,14 @@ class Robot(Entite):
         self.direction = 0
         self.unite_de_temps = 10 #inutile pour le moment
 
-    def __repr__(self):
-        return "C'est le robot d'identifiant " + str(self.id) + " se trouve en (" + str(self.x) + "," + str(self.y) + ")" + " et est tourné de " + str(self.direction) + "°"
-    
     #à déterminer selon la taille des roues? Pour l'instant 1 tour = 10cm 
     #Et on part du principe que la roue tourne à 6 tour / min (ce qui est très lent)
 
     #Les instructions pour faire tourner les roues sont a déterminer, 
-        #Les roues a priori tourneront tout le temps à la même vitesse, mais pas nécessairement dans le même sens
-        #Il faut donc déterminer le nb de tours à faire pour parcourir une distance donnée
-        #Ces fonctions vous permettront de faire tourner les roues dans un sens ou dans l'autre
-        #Il faudt les appeler dans les méthodes avancer, reculer, tourner_a_gauche et tourner_a_droite
+    #Les roues a priori tourneront tout le temps à la même vitesse, mais pas nécessairement dans le même sens
+    #Il faut donc déterminer le nb de tours à faire pour parcourir une distance donnée
+    #Ces fonctions vous permettront de faire tourner les roues dans un sens ou dans l'autre
+    #Il faudt les appeler dans les méthodes avancer, reculer, tourner_a_gauche et tourner_a_droite
     #Fin des méthodes pour faire tourner les roues
 
 
@@ -39,9 +36,11 @@ class Robot(Entite):
 
         self.x += round( distance * cos(radians(self.direction)) , 10)
         self.y += round( distance * sin(radians(self.direction)) , 10)
-        #Mettre à jour les coordonnées x et y (triginonmétrie)
+        #Mettre à jour les coordonnées x et y en fonction de la direction et de la distance
+        #arrondit les résultats à 10 chiffres après la virgule
 
     def reculer(self, distance):
+        #calcule le nb de tours nécessaires pour parcourir la distance donnée
         nb_tours = float(distance)/10
 
         #Il faut les faire tourner en même temps
@@ -52,8 +51,9 @@ class Robot(Entite):
 
         self.x -= round( distance * cos(radians(self.direction)) , 10)
         self.y -= round( distance * sin(radians(self.direction)) , 10)
+        #Mettre à jour les coordonnées x et y en fonction de la direction et de la distance
+        #arrondit les résultats à 10 chiffres après la virgule
 
-        #Mettre à jour les coordonnées
 
     def tourner_a_gauche(self, angle):
         #calcule le nb de tours pour tourner à gauche de l'angle donné
