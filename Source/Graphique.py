13f83b3a858c7398e6530233285d79f6e0b7d5e2
SniKoxx <huang.loic2004@gmail.com>
2024-01-26 16:13:33 +0100

    Modification du Scale + test

--- Source/Graphique.py
+++ Source/Graphique.py
@@ -6,18 +6,28 @@ window = Tk()
 longueur = 1080
 largeur = 720
 window.title("Simulation")
-window.geometry(f"{longueur+200}x{largeur+5}")
-Vitesse = IntVar()
+window.geometry(f"{longueur+250}x{largeur+5}")
 window.resizable(height=False, width=False)
-frame = Frame(window)
-frame.pack(fill = Y,side = RIGHT)
+
+
+frame = Frame(window,borderwidth=5, relief="raise")
+frame.pack(fill = BOTH,side = RIGHT)
+Vitesse = IntVar()
+
 rec_base = Canvas(window, bg='blue', width=longueur, height=largeur)
 rec_base.place(x='0',y='0')
-boutton_moins_distance = Button(frame, text="-10",font =("verdana", 13), fg='black', bg='white')
-boutton_moins_distance.pack(side = LEFT)
-scale = Scale(frame, from_=0, to=100, variable=Vitesse, orient=HORIZONTAL)
-scale.pack(pady=20,side = RIGHT)
+
+scale = Scale(frame, from_=0, to=100, length=240,variable=Vitesse, orient=HORIZONTAL)
+scale.pack(pady=20,side = BOTTOM)
+
 text2 = Label(frame, text="Vitesse", font=("Courrier",40), fg="black")
 text2.pack(side = BOTTOM)
 
 window.mainloop()
+
+
+
+
+
+#boutton_moins_distance = Button(frame, text="-10",font =("verdana", 13), fg='black', bg='white')
+#boutton_moins_distance.pack(side = LEFT)
