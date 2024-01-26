c09f53dedbba6a35b53dec1a8275eb18b96e7d9b
YanisWK <yanis.saadi03@gmail.com>
2024-01-26 14:55:06 +0100

    Creation classe entite

--- Test/Test_Robot.py
+++ Test/Test_Robot.py
@@ -1,8 +1,4 @@
 from Robot import *
-<<<<<<< HEAD
-
-=======
->>>>>>> fedd047aa38d3cd32a4f5c07865148b5c238fff6
 from math import *
 
 rb = Robot(12)
@@ -29,7 +25,6 @@ print("\n")
 
 print("Le robot tourne à droite de 90° vers la droite et recule de 10cm:")
 rb.tourner_a_droite(90)
-<<<<<<< HEAD
 
 rb = Robot(12)
 print(rb)
@@ -38,7 +33,5 @@ print(rb)
 rb.tourner_a_droite(25)
 print(rb)
 
-=======
->>>>>>> fedd047aa38d3cd32a4f5c07865148b5c238fff6
 rb.reculer(10)
 print(rb)
