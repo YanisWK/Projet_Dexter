330bbf333db297b04eeca7cfdf32b31591734fd5
LuckeMao <maolucke@gmail.com>
2024-01-30 14:55:23 +0100

    Adaptation des test du robot

--- Source/Test_Robot.py
+++ Source/Test_Robot.py
@@ -25,13 +25,5 @@ print("\n")
 
 print("Le robot tourne à droite de 90° vers la droite et recule de 10cm:")
 rb.tourner(-90)
-
-rb = Robot(12)
-print(rb)
-rb.avancer(10)
-print(rb)
-rb.tourner(25)
-print(rb)
-
 rb.avancer(-10)
 print(rb)
