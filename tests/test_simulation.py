4e714e8fb7f7283db4239341ea54bf37e8f0d2e0
Victor Roque Gomes <victorloic22@gmail.com>
2024-02-02 15:46:32 +0100

    modification de test_simulation.py en test unitaire et ajout dans ce dernier de la vérificztion de l'instantation de ce dernier

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -6,5 +6,5 @@ import unittest
 class TestSimulation(unittest.TestCase):
     def test_simulation_est_instancce_de_simulation(self):
         robot = Robot("Dexter",50,25,0,0)
-        simulation = Simulation(1,robot,1000,700,60)
-        self.assertIsInstance(simulation, Simulation)
+        test_simulation = Simulation(1,robot,1000,700,60)
+        self.assertIsInstance(test_simulation, Simulation)
