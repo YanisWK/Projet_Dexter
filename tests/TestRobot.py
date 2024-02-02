4e3c19865122903f8668d1a264f59fd1130031e1
Victor Roque Gomes <victorloic22@gmail.com>
2024-02-02 10:39:21 +0100

    verfification de l'insta (TestRobot.py)

--- tests/TestRobot.py
+++ tests/TestRobot.py
@@ -1,5 +1,6 @@
 import unittest
 
 class TestRobot(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_robot_est_instancce_de_robot(self):
+        robot = Robot()
+        self.assertIsInstance(robot, Robot)
