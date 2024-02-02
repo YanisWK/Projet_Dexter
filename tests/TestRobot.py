83a480bd55d6896ec393b05b07621f32c4afc12c
Victor Roque Gomes <victorloic22@gmail.com>
2024-02-02 12:19:23 +0100

    résolution de l'importation de Robot.py dans TestRobot.py (ça ma pris trop de temps T_T) et premier test unitaire de Robot.py

--- tests/TestRobot.py
+++ tests/TestRobot.py
@@ -1,16 +1,27 @@
+import unittest
 import sys
-sys.path.append("..")
-from Source.Robot import Robot
+import os
+
+chemin_actuel = os.path.dirname(os.path.abspath(__file__))
+chemin_parent = os.path.dirname(chemin_actuel)
+chemin_source = os.path.join(chemin_parent, 'Source')
+sys.path.append(chemin_source)
+
+from Robot import Robot 
 
 import unittest
 
 class TestRobot(unittest.TestCase):
     def test_robot_est_instancce_de_robot(self):
-        robot = Robot()
+        robot = Robot("Dexter",50,25,0,0)
         self.assertIsInstance(robot, Robot)
 
     def test_robot_avancer(self):
-        robot = Robot()
+        robot = Robot("Dexter",50,25,0,0)
         robot.avancer(10)
         self.assertEqual(robot.getX(), 10)
         self.assertEqual(robot.getY(), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+    
