a7e651f25c4df36d3c74d8d3e92c5537f80de993
vntgogh <ines.ediane29@gmail.com>
2024-02-08 23:32:31 +0100

    independance de simu par rapport à graphique

--- Dexturret/main.py
+++ Dexturret/main.py
@@ -1,4 +1,15 @@
-from Source.Robot import Robot
+from Turret.robot import Robot
+from Turret.simulation import Simulation
+from main_interface2D import main
 
 Robot = Robot("Dexter", 50, 25, 0, 0)
 print(Robot)
+
+larg = 700
+long = 1000
+robot1 = Robot(1,50,25,long/2,larg/2)
+simu = Simulation(1,robot1,larg,long,60)
+
+#permet les main de l'interface et de la simulation
+if __name__ == "__main__":
+    main()
---
+++ Dexturret/main_interface2D.py
@@ -0,0 +1,4 @@
+from tkinter import Tk
+from Interface.graphique import window
+
+window.mainloop()
