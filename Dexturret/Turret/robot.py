6851e68328327735ef122d5e2004ef28f752e80a
vntgogh <ines.ediane29@gmail.com>
2024-02-09 00:25:09 +0100

    update docu de robot.py

--- Dexturret/Turret/robot.py
+++ Dexturret/Turret/robot.py
@@ -2,26 +2,24 @@ from math import cos, sin, radians
 
 """Documentation : 
 
-    Description generale : Fichier contenant la classe Robot avec ces attributs de position,direction ainsi que sa longueur et largeur
+    Description generale : Fichier contenant la classe Robot, les fonctions de mises à jour de coordonnées et de déplacement
     Listes des methodes :
     - __init__ => crée une instance Robot
                   la classe utilise des listes velociteD et velociteR, pour stocker les déplacements et les 
                   rotations à chaque rafraîchissement
-    -avancer =>
-    -tourner =>
+    - avancer => fait avancer le robot sur une distance donnée
+    - tourner => fait tourner le robot dans un angle donné
     - coeff_directeur => retourne le couple (a,b) de la droite directionelle ax+by
                     Ces coefficients sont les composantes du vecteur unitaire dirigeant le robot  
                     - a est la composante horizontale (abscisse)
                     - b est la composante verticale (ordonnée)
                     Ils décrivent la direction associée à l'angle dans le cercle trigonométrique.
+    - deplacementRobot => effectue les déplacements du robot avec les fonctions avancer et tourner
 
-    - rafraichir => effectue les déplacements avancer/tourner à l'aide des listes velociteD et velociteR et 
-                    gère les collisions avec les bords de l'environnement
+    - rafraichir => met à jour les déplacements et positions des coins du robot sur un temps de rafraichissement donné
 
     - pos_coins_Robot => calcule la position des 4 coins à l'aide de la direction et de la taille du robot
 
-    - deplacementRobot => fait avancer ou reculer le robot dans sa direction actuelle, 
-                        en fonction des déplacements contenus dans velociteD et en fonction de la vitesse
 
 """
 
@@ -47,9 +45,9 @@ class Robot:
         self.vitesseRoueG = 0
         self.vitesseRoueD = 0
 
-        self.pret = False  #Si la simulation est en pause et le robot est prêt, le robot se rafraichit 
+        self.pret = False  #La simulation est activée et le robot est en mouvement
 
-        #Les 4 coins du robot delon la position du centre et la taille du robot
+        #Les 4 coins du robot selon la position du centre et la taille du robot
         L = self.longueur / 2
         l = self.largeur / 2
         x = self.x
@@ -62,18 +60,33 @@ class Robot:
         return "C'est le robot d'identifiant " + str(self.id) + " qui se trouve en (" + str(self.x) + "," + str(self.y) + ")" + " et est tourné de " + str(self.direction) + "°"
 
 
-    #Fonction non réaliste permettant de simuler les mouvements du robot
     def avancer(self, distance):
+        """
+        Déplace le robot en ligne droite en mettant à jour les coordonnées de robot 
+
+        Paramètre :
+        - distance : distance à parcourir
+        
+        """
         print("Le Robot a avancé de ", distance, "cm")
 
+        #mise à jour des coordonnées grâce aux fonctions cosinus/sinus
         self.x += round( distance * cos(radians(self.direction)) , 10)
         self.y -= round( distance * sin(radians(self.direction)) , 10)
 
 
     def tourner(self, angle):
+        """
+        Effectue une rotation en ajustant la direction pour rester dans [0, 360]
+        si l'angle n'est pas compris dans [0,360], c'est le modulo 360 sur cet angle qui est ajouté 
+        à la direction actuelle
+
+        Paramètre :
+        - angle : angle de rotation
+
+        """
         self.direction += angle
 
-        #ajuste la direction pour rester dans [0, 360)
         if (self.direction > 360):
             self.direction -= 360
         if (self.direction < 0):
@@ -95,9 +108,7 @@ class Robot:
    
     def pos_coins_Robot(self):
         """
-        - Calcule, à l'aide des dimensions du robot et de la direction, la position des 4 coins du robot
-        - Nécessite obtenirAngle
-        - Le centre du rectangle est la position x y du robot
+        - Calcule la position des 4 coins du robot à l'aide des dimensions du robot et de la direction
         - Modifie self.coordRobot par la liste des coordonnees des 4 coins
 
         """
@@ -116,29 +127,33 @@ class Robot:
     
     def deplacementRobot(self, temps):
         """
-        Effectue les calculs nécessaires afin de déplacer le robot :
-        - Ajoute dans velociteD les distances à faire à chaque rafraichissement en fonction de la distance 
-        à parcourir et la vitesse, dans la direction du robot
+        Calcule la distance de déplacement en ligne droite et la rotation du robot à chaque rafraîchissement, 
+        puis met à jour la position et la direction du robot en appelant avancer et tourner
+        
+        Paramètre :
+        - temps : temps par rafraichissement
+        
+        """       
 
-        """
-
-        #Calcule de la distance que parcoure le robot à chaque rafraîchissement
-        #distance_par_rafraichissement = vitesse / temps
+        #Calcul de la distance que parcourt le robot à chaque rafraîchissement distance_par_rafraichissement = vitesse/temps
 
         vitesse_deplacement = (self.vitesseRoueG + self.vitesseRoueD) / 2
         deplacement_par_rafraichissement = vitesse_deplacement / temps
         self.avancer(deplacement_par_rafraichissement)
 
-        #Calcule de la rotation que le robot doit tourner à chaque rafraîchissement
+        #Calcul de la rotation que le robot doit faire à chaque rafraîchissement
 
         vitesse_rotation = self.vitesseRoueD - self.vitesseRoueG
         rotation_par_rafraichissement = vitesse_rotation / temps
         self.tourner(rotation_par_rafraichissement)
         
 
-    def rafraichir(self,temps):
+    def rafraichir(self,fps):
         """
         Met à jour les positions des coins du robot et les déplacements du robot
+
+        Paramètre :
+        - fps : 
         """
-        self.deplacementRobot(temps)
+        self.deplacementRobot(fps)
         self.pos_coins_Robot()
