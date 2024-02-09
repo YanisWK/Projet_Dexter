0f9b884a7db90f36ed61abd3cda3a5b01e213af6
YanisWK <yanis.saadi03@gmail.com>
2024-02-09 16:07:02 +0100

    ajout du module controller et du fichier control.py

---
+++ Dexturret/controller/control.py
@@ -0,0 +1,12 @@
+class Avancer():
+    def __init__(self,distance) -> None:
+        pass
+
+class Tourner():
+    def __init__(self,angle) -> None:
+        pass
+
+class TracerCarre():
+    def __init__(self,distance) -> None:
+        pass
+
