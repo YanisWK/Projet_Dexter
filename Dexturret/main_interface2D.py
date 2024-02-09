cc58ba9d99586537daf863186f1a57826a17ac74
LuckeMao <maolucke@gmail.com>
2024-02-09 10:25:51 +0100

    Ajout de l'import du robot et de la simulation, ainsi que leur création

--- Dexturret/main_interface2D.py
+++ Dexturret/main_interface2D.py
@@ -1,4 +1,12 @@
 from tkinter import Tk
-from Interface.graphique import window
+#from Interface.graphique import window
+from Turret import Robot
+from Turret import Simulation
 
-window.mainloop()
+larg = 700
+long = 1000
+robot1 = Robot(1,50,25,long/2,larg/2)
+simu = Simulation(1,robot1,larg,long,60)
+
+
+#window.mainloop()
