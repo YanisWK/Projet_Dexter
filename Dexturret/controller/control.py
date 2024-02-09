a3278d1d8161fba2579e84c6bec66a6f32b9bd24
YanisWK <yanis.saadi03@gmail.com>
2024-02-09 16:26:52 +0100

    control.py ajout

--- Dexturret/controller/control.py
+++ Dexturret/controller/control.py
@@ -1,11 +1,16 @@
 class Avancer():
     def __init__(self,distance) -> None:
-        pass
+        self.distance=distance
+
 
 class Tourner():
     def __init__(self,angle) -> None:
         pass
 
+class DeplacerVers():
+    def __init__(self,x,y):
+        pass
+    
 class TracerCarre():
     def __init__(self,distance) -> None:
         pass
