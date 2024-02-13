64462acafe58da58cb460afa2ac1a395004481c5
YanisWK <yanis.saadi03@gmail.com>
2024-02-13 15:44:13 +0100

    Ajout dans control.py

--- Dexturret/controller/control.py
+++ Dexturret/controller/control.py
@@ -9,20 +9,28 @@ class Avancer():
     def etape(self):
         self.parcouru+=self.distance #il me faudrait la distance parcouru poure chaque etapes ou j'ai juste mal compris peut être la distance parcouru par le robot d'ailleurs
         if self.stop() : return
-
     def stop(self):
-        return self.parcouru>=self.distance
+        return self.parcouru>self.distance
     
 
 class Tourner():
     def __init__(self,angle):
         self.angle=angle
+    def start(self):
+        self.angle_parcouru=0
+    def etape(self):
+        self.angle_parcouru+=self.angle 
+        if self.stop(): return
+    def stop(self):
+        return self.angle_parcouru>self.angle
+    
 
-class DeplacerVers():
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
 class TracerCarre():
-    def __init__(self,distance):
-        self.distance=distance
+    def __init__(self,tailleCote):
+        stratAvancer=Avancer(tailleCote)
+        stratTourner=Tourner(tailleCote)
+        self.strats=[stratAvancer,stratTourner]
+        self.tailleCote=tailleCote
+        self.current=-1
+
 
