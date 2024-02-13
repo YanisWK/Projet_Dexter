0004a8a4da2b1e31675770be9180e4f597739c20
vntgogh <ines.ediane29@gmail.com>
2024-02-13 17:33:19 +0100

    correction de l'appel de la fonction de détection dans le main de l'interface raphique

--- Dexturret/main_interface2D.py
+++ Dexturret/main_interface2D.py
@@ -34,7 +34,7 @@ scale_roue_droite = creer_scale(frame, "Vitesse roue droite", vitesse_roue_droit
 scale_roue_droite.pack(ipady=20)
 
 def affichage_distance(long,larg):
-    text_distance.config(text = f"Distance : {robot.detect_distance(long,larg)}")
+    text_distance.config(text = f"Distance : {robot.detect_distance(simu)}")
     print("SA MARCHE OU PAS SALO")
 
 text_distance = Label(frame, text="Distance : 0.0")
