58e454ecbd248564947418cfd659c15e79e78f01
LuckeMao <maolucke@gmail.com>
2024-02-16 14:49:48 +0100

    Création du __init__.py dans Dexturret

---
+++ Rift/Dexturret/__init__.py
@@ -0,0 +1,2 @@
+from .Interface import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color
+from .Turret import robot, simulation
