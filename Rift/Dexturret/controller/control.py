06da3ebf46e631240d3f4b90af0bfac2ec62fb7d
LuckeMao <maolucke@gmail.com>
2024-02-26 09:33:06 +0100

    Ajout du robot et de la vitesse dans les paramètres du contrôleur

--- Rift/Dexturret/controller/control.py
+++ Rift/Dexturret/controller/control.py
@@ -1,45 +1,71 @@
+import Turret
 update_time=60
 
 
 class Avancer():
-    def __init__(self,distance):
-        self.distance=distance
+    def __init__(self, robot, distance, vitesse):
+        self.robot = robot
+        self.distance = distance
+        self.vitesse = vitesse
+
     def start(self):
         self.parcouru=0
+
     def etape(self):
-        self.parcouru+=self.distance #il me faudrait la distance parcouru poure chaque etapes ou j'ai juste mal compris peut être la distance parcouru par le robot d'ailleurs
-        if self.stop() : return
+        self.robot.vitesse_lineaire_roue_gauche = self.vitesse
+        self.robot.vitesse_lineaire_roue_droite = self.vitesse
+        self.parcouru+=self.distance #Comment obtenir la relle distance parcourue?
+        if self.stop() :
+            self.robot.vitesse_lineaire_roue_gauche = 0
+            self.robot.vitesse_lineaire_roue_droite = 0
+            return
+        
     def stop(self):
-        return self.parcouru>=self.distance
+        return self.parcouru >= self.distance
     
 
 class Tourner():
-    def __init__(self,angle):
+    def __init__(self, robot, angle, vitesse):
+        self.robot = robot
         self.angle=angle
+        self.vitesse = vitesse
+
     def start(self):
-        self.angle_parcouru=0
+        self.angle_parcouru = 0
+
     def etape(self):
-        self.angle_parcouru+=self.angle 
-        if self.stop(): return
+        self.robot.vitesse_lineaire_roue_gauche = self.vitesse
+        self.robot.vitesse_lineaire_roue_droite = -self.vitesse
+        self.angle_parcouru += self.angle #Comment obtenir l'angle que le robot a reellement parcouru?
+
+        if self.stop():
+            self.robot.vitesse_lineaire_roue_gauche = 0
+            self.robot.vitesse_lineaire_roue_droite = 0
+            return
+
     def stop(self):
-        return self.angle_parcouru>=self.angle
+        return self.angle_parcouru >= self.angle
     
 
 class TracerCarre():
     def __init__(self,tailleCote):
-        stratAvancer=Avancer(tailleCote)
-        stratTourner=Tourner(90) #Besoin de savoir a chaque fois qu'on tourne de combien de degrès il faut tourner.
+        stratAvancer = Avancer(tailleCote)
+        stratTourner = Tourner(90) #Besoin de savoir a chaque fois qu'on tourne de combien de degrès il faut tourner.
 
-        self.strats=[stratAvancer,stratTourner,stratAvancer,stratTourner,stratAvancer,stratTourner,stratAvancer,stratTourner]
+        self.strats = [stratAvancer,stratTourner,stratAvancer,stratTourner,stratAvancer,stratTourner,stratAvancer,stratTourner]
         self.tailleCote=tailleCote
-        self.current=-1
+        self.current = -1
+
     def start(self):
-        self.current=-1
+        self.current = -1
+
     def etape(self):
-        if self.stop():return
-        if self.current<0 or self.strats[self.current].stop():
-            self.current+=1
+        if self.stop():
+            return
+        if self.current < 0 or self.strats[self.current].stop():
+            self.current += 1
             self.strats[self.current].start()
-        self.strats[self.current].step
+        self.strats[self.current].etape()
+
     def stop(self):
-        return self.current==len(self.starts)-1 and self.strats[self.current].stop()
+        return self.current == len(self.strats)-1 and self.strats[self.current].stop()
