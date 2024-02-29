a85d7f5cc67ee56ccb084c161958081f70bc347f
vntgogh <ines.ediane29@gmail.com>
2024-02-29 20:39:51 +0100

    tests unitaires manquants de robot.py

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,4 +1,5 @@
-from src.robot import Robot 
+from Dexturret.Turret.robot import Robot 
+from math import cos, sin, radians
 
 import unittest
 
@@ -30,6 +31,20 @@ class TestRobot(unittest.TestCase):
         test_robot.deplacementRobot(1)
         self.assertEqual(test_robot.x, 0)
         self.assertEqual(test_robot.y, 0)
+
+def test_robot_coeff_directeur(self):
+    test_robot = Robot("Dexter", 50, 25, 0, 0)
+    res = test_robot.coeff_directeur(45) #angle = 45
+    res_assert = (cos(radians(45)),sin(radians(45)))
+    self.assertEqual(res, res_assert)
+
+def test_robot_detect_distance(self):
+    test_robot = Robot("Dexter", 50, 25, 0, 0)
+    simu_longueur = 1000
+    simu_largeur = 700
+    res = test_robot.detect_distance(simu_longueur, simu_largeur)
+    res_assert = round(simu_longueur/2,simu_largeur/2)
+    self.assertEqual(res, res_assert)
 
 if __name__ == '__main__':
     unittest.main()
