2fce44d58a34dd0628b3ddea714e3818e0a5a109
Victor Roque Gomes <victorloic22@gmail.com>
2024-03-05 23:25:45 +0100

    Suppresion d'un __inti__.py inutile

--- Rift/Dexturret/__init__.py
+++
@@ -1,3 +0,0 @@
-from .interface import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color
-from .turret import robot, simulation
-from .controller import Avancer, Tourner, TracerCarre
