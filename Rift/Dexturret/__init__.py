f8f99f00a83af4d30b3a76ea6559e041c6438a9a
LuckeMao <maolucke@gmail.com>
2024-03-19 20:05:17 +0100

    Suppression de TracerCarre dans les import des init

--- Rift/Dexturret/__init__.py
+++ Rift/Dexturret/__init__.py
@@ -1,3 +1,3 @@
 from Dexturret.interface import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color
 from Dexturret.turret import robot, simulation
-from Dexturret.controller import AvancerRobot, TournerRobot, TracerCarre
+from Dexturret.controller import AvancerRobot, TournerRobot
--- Rift/Dexturret/controller/__init__.py
+++ Rift/Dexturret/controller/__init__.py
@@ -1,2 +1,2 @@
-from Dexturret.controller.control import AvancerRobot, AvancerViteRobot, TournerRobot, TracerCarre, AvancerViteMur, Instructions
+from Dexturret.controller.control import AvancerRobot, AvancerViteRobot, TournerRobot, Instructions
 from Dexturret.controller.adaptateur import adaptateurIRL, adaptateurSimu
