f5c1e76173ae7b8cb6e494f6031493b0fe4cb6b2
SniKoxx <huang.loic2004@gmail.com>
2024-03-19 14:50:41 +0100

    Possibilité de choisir entre le robotSimu et robotIRL et refactorisation du code

--- Rift/main_controller.py
+++ Rift/main_controller.py
@@ -8,17 +8,36 @@ import Dexturret.controller as controller
 #Configuration des logs 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S", filemode="w",filename="test.log")
 
-#Initialisation des paramètres du robot et de la simulation
+#Initialisation des paramètres des robot et des variables pour la simulation
 larg = 700
 long = 1000
-#robotAdapt = controller.adaptateurSimu(1, 50, 25, 0.05, long/2, larg/2, time())
-robotAdapt = controller.adaptateurIRL()
-robotAdapt.direction = 135
-robotAdapt.pret = True
+robotSimu = controller.adaptateurSimu(1, 50, 25, 0.05, long/2, larg/2, time())
+robotIRL = controller.adaptateurIRL()
 fps = 60
+
+
+#Initialisation du robot qu'on va utiliser dans le suite du programme
+try:
+    choix = int(input("Quel robot vous voulez désigner ? (Tapez 1 pour le robotSimu et 2 pour le robotIRL) : "))
+    if choix == 1:
+        robotAdapt = robotSimu
+        robotAdapt.direction = 135
+        robotAdapt.pret = True
+        simu = turret.Simulation(1, robotAdapt, larg, long, fps)
+        window, couleur, canvas, frame, text_distance = interface.creer_graphique(robotAdapt,simu)
+        refresh = simu
+    elif choix == 2:
+        robotAdapt = robotIRL
+        refresh = robotAdapt
+    else:
+        print("Arrête du programme\n")
+        exit()
+except ValueError:
+    print("Arrête du programme \n")
+    exit()
+
 simu = turret.Simulation(1, robotAdapt, larg, long, fps)
-
-
+#Initialisation des stratégies séquentielle 
 stratAvancer = controller.AvancerRobot(robotAdapt, 100, 100, fps)
 stratTournerDroite = controller.TournerRobot(robotAdapt, -90, fps)
 stratTournerGauche = controller.TournerRobot(robotAdapt, 90, fps)
@@ -30,78 +49,50 @@ strats = [stratAvancer, stratTournerDroite, stratAvancer, stratTournerDroite, st
 
 carre = [stratAvancer, stratTournerDroite, stratAvancer, stratTournerDroite, stratAvancer, stratTournerDroite, stratAvancer, stratTournerDroite]
 
-
-boucle = True
-boucle = False #a supprimer
-'''
-while (boucle):
-    print("Choix du controller:\n(1) Tracer un carré\n(2) Foncer vers un mur")
-    choix = int(input())
-
-    if (choix != 1 and choix != 2):
-        print("Choix invalide, veuillez recommencer")
-    else:
-        print("Entrez la vitesse du robot:")
-        vitesse = int(input())
-        if not (isinstance(vitesse, int)):
-            print("Entrée invalide, veuillez recommencer depuis le début (l'entrée doit être un entier)")
-        else:
-            if (choix == 1):
-                print("Entrez la distance d'un coté du carré:")
-                distance = int(input())
-                if not (isinstance(distance, int)):
-                    print("Entrée invalide, veuillez recommencer depuis le début (l'entrée doit être un entier)")
-                else:
-                    controller_choisi = controller.TracerCarre(robotAdapt, distance, vitesse, fps)
-            else:
-                controller_choisi = controller.AvancerViteMur(robotAdapt, simu, vitesse, fps)
-            boucle = False
-'''
-
-#window, couleur, canvas, frame, text_distance = interface.creer_graphique(robotAdapt,simu)
-
 controller_choisi = controller.Instructions(carre)
 
-robotAdapt.dernier_rafraichissement = time()
-tab = [(robotAdapt.x,robotAdapt.y)]
-tailleMax = 500
+
+if (robotAdapt == robotSimu):
+    robotAdapt.dernier_rafraichissement = time()
+    tab = [(robotAdapt.x,robotAdapt.y)]
+    tailleMax = 500
+
+
 #Boucle principale de la simu
 while simu.awake and not controller_choisi.stop():
 
     #Mise a jour tous les 1/temps
-    sleep(1/simu.fps)
+    sleep(1/fps)
 
     controller_choisi.etape()
     
-    #On efface tout et on redessine le robot
+    refresh.rafraichir()
+    if (robotAdapt == robotSimu):
+        interface.rafraichir_graphique(simu, canvas)
 
-    #simu.rafraichir()
-    robotAdapt.rafraichir()
-
-    #interface.rafraichir_graphique(simu, canvas)
-
-    #Affichage de la ligne rouge pour la direction du robot
-    #canvas.pack()
-    if (robotAdapt.x,robotAdapt.y) != tab[-1]:
-        if len(tab) > tailleMax:
-            tab.pop(0)
-        tab.append((robotAdapt.x,robotAdapt.y))
-    for elem in range(1,len(tab)):
-        x,y = tab[elem-1]
-        x1,y1 = tab[elem]
-        #canvas.create_line(x, y, x1, y1, fill="black")
-        #canvas.pack()
-    #canvas.update()
-    #interface.affichage_distance(text_distance,robotAdapt,simu.longueur,simu.largeur)
+        #Affichage de la ligne rouge pour la direction du robot
+        canvas.pack()
+        if (robotAdapt.x,robotAdapt.y) != tab[-1]:
+            if len(tab) > tailleMax:
+                tab.pop(0)
+            tab.append((robotAdapt.x,robotAdapt.y))
+        for elem in range(1,len(tab)):
+            x,y = tab[elem-1]
+            x1,y1 = tab[elem]
+            canvas.create_line(x, y, x1, y1, fill="black")
+            canvas.pack()
+        canvas.update()
+        interface.affichage_distance(text_distance,robotAdapt,simu.longueur,simu.largeur)
 
 #Affichage d'une fenêtre pop-up en cas de collision
-"""
-if not simu.awake:
-    interface.popup_collision(window)
-    logging.info(f'Le Robot est entré en collision avec un obstacle')
-"""
+if (robotAdapt == robotIRL):
+    print("Fin du programme robot IRL")
+    exit()
 
-#Lancement de la boucle principale
-print("ok")
-#if not boucle:
-    #window.mainloop()
+interface.popup_collision(window)
+logging.info(f'Le Robot est entré en collision avec un obstacle')
+
+print("Fin du programme robotSimu")
+
+
+
