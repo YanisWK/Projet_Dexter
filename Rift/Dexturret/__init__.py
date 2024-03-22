3e223239bf428fbfd3ae05a3bc1df9a07ce59dcc
LuckeMao <maolucke@gmail.com>
2024-03-22 15:34:04 +0100

    Création du robot simu et irl dans le __init__ de Dexturret

--- Rift/Dexturret/__init__.py
+++ Rift/Dexturret/__init__.py
@@ -1,5 +1,5 @@
 from Dexturret.interface import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color
-from Dexturret.turret import robot, simulation
+from Dexturret.turret import Robot, Robot2IN013Fake, simulation
 from Dexturret.controller import AvancerRobot, TournerRobot, adaptateurSimu, adaptateurIRL
 
 from time import time
@@ -8,12 +8,16 @@ from time import time
 larg = 700
 long = 1000
 fps=60
-robotSimu = adaptateurSimu(1, 50, 25, 5, long/2, larg/2, fps)
-robotIRL = adaptateurIRL()
 
-stratAvancer= AvancerRobot(robot, 100, 100, fps)
-stratTournerDroite = TournerRobot(robot, -90, fps)
-stratTournerGauche = TournerRobot(robot, 90, fps)
+robotSim = Robot(1, 50, 25, 5, long/2, larg/2, time())
+robotSimu = adaptateurSimu(robotSim)
+
+robotFake = Robot2IN013Fake()
+robotIRL = adaptateurIRL(robotFake)
+
+stratAvancer= AvancerRobot(robotSimu, 100, 100, fps)
+stratTournerDroite = TournerRobot(robotSimu, -90, fps)
+stratTournerGauche = TournerRobot(robotSimu, 90, fps)
 
 carre = [stratAvancer, stratTournerDroite, stratAvancer, stratTournerDroite, stratAvancer, 
          stratTournerDroite, stratAvancer, stratTournerDroite]
