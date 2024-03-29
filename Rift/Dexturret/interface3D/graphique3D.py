0065fdca554843342595a0aa469c70897028aa86
SniKoxx <huang.loic2004@gmail.com>
2024-03-29 15:39:51 +0100

    Ajout d'un model robot

--- Rift/Dexturret/interface3D/graphique3D.py
+++ Rift/Dexturret/interface3D/graphique3D.py
@@ -6,8 +6,15 @@ class VueRobot(ShowBase):
     def __init__(self):
         super().__init__()
 
+class test(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
 
-app = VueRobot()
+        self.building = loader.loadModel("test.glb")
+        self.building.setPos(0,50,0)
+        self.building.reparentTo(render)
+
+app = test()
  
 print(__builtins__.base)
 
