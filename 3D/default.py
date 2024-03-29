dba8f12e8bdbc11e9521e724c424ead19a8272b2
Victor Roque Gomes <victorloic22@gmail.com>
2024-03-29 14:03:44 +0100

    Mise à jour de l'initialisation de la classe VueRobot dans default.py

--- 3D/default.py
+++ 3D/default.py
@@ -2,10 +2,8 @@ from direct.showbase.ShowBase import ShowBase
 
 class VueRobot(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
-        self.scene = self.loader.loadModel("models/robot")
-        self.scene.reparentTo(self.render)
-        self.scene.setScale(0.5, 0.5, 0.5)
-        self.scene.setPos(8, 42, 0)
+        super().__init__()
 
-VueRobot().run()
+app = VueRobot()
+
+app.run()
