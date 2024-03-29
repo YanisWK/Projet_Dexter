d00d348e75557a0c0bbd904614cff23c7c8d5543
YanisWK <yanis.saadi03@gmail.com>
2024-03-29 16:12:20 +0100

    "Ajout du robot dans une jungle"

--- Rift/Dexturret/interface3D/graphique3D.py
+++ Rift/Dexturret/interface3D/graphique3D.py
@@ -1,20 +1,27 @@
 from panda3d.core import loadPrcFile
 loadPrcFile("config/conf.prc")
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 class VueRobot(ShowBase):
     def __init__(self):
         super().__init__()
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(0,50,0)
+        self.scene.reparentTo(self.render)
 
-class test(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
+        self.robot = Actor("test.glb")
+        self.robot.setScale(0.01, 0.01, 0.01)
+        self.scene.setPos(-8, 58, 0)
+        self.robot.reparentTo(self.render)
 
-        self.building = loader.loadModel("test.glb")
-        self.building.setPos(0,50,0)
-        self.building.reparentTo(render)
 
-app = test()
+app = VueRobot()
  
 print(__builtins__.base)
 
