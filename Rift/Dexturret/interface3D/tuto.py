f099fdcb5a7aeeda238ca27b65a9dece1053a2b7
YanisWK <yanis.saadi03@gmail.com>
2024-03-29 14:26:20 +0100

    ajout reel du du fichier tuto.py

--- Rift/Dexturret/interface3D/tuto.py
+++ Rift/Dexturret/interface3D/tuto.py
@@ -3,11 +3,16 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        # Disable the camera trackball controls.
+        self.disableMouse()
 
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
@@ -27,6 +32,27 @@ class MyApp(ShowBase):
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
         self.pandaActor.loop("walk")
+
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                   Point3(0, -10, 0),
+                                                   startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                   Point3(0, 10, 0),
+                                                   startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                   Point3(180, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                   Point3(0, 0, 0),
+                                                   startHpr=Point3(180, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                  posInterval2, hprInterval2,
+                                  name="pandaPace")
+        self.pandaPace.loop()
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
