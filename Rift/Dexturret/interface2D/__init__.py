5fb3b6e25addc07c805b4f539b9760d221d20b84
YanisWK <yanis.saadi03@gmail.com>
2024-03-29 14:13:23 +0100

    "Ajout d'une scence avec des bambou et modification des init pour l'interface 2D"

--- Rift/Dexturret/interface2D/__init__.py
+++ Rift/Dexturret/interface2D/__init__.py
@@ -1,1 +1,1 @@
-from Dexturret.interface.graphique import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color, creer_graphique, affichage_distance, onKeyPress, dessiner
+from Dexturret.interface2D.graphique import creer_canvas, creer_couleur, creer_fenetre, creer_frame, creer_scale, affiche_robot, popup_collision, rafraichir_graphique, change_color, creer_graphique, affichage_distance, onKeyPress, dessiner
--- Rift/Dexturret/interface3D/default.py
+++ Rift/Dexturret/interface3D/default.py
@@ -1,9 +1,19 @@
 from direct.showbase.ShowBase import ShowBase
 
-class VueRobot(ShowBase):
+
+class MyApp(ShowBase):
+
     def __init__(self):
-        super().__init__()
+        ShowBase.__init__(self)
 
-app = VueRobot()
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
 
+
+app = MyApp()
 app.run()
