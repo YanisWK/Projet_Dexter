91fc57817fefecbf7089d36ec1d3406cba5aebe2
vntgogh <ines.ediane29@gmail.com>
2024-03-29 16:15:16 +0100

    ajout d'une scene

--- Rift/Dexturret/interface3D/graphique3D.py
+++ Rift/Dexturret/interface3D/graphique3D.py
@@ -5,17 +5,20 @@ from direct.showbase.ShowBase import ShowBase
 class VueRobot(ShowBase):
     def __init__(self):
         super().__init__()
+       
 
 class test(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
         self.building = loader.loadModel("test.glb")
-        self.building.setPos(0,50,0)
+        self.building.setPos(0,5,0)        
         self.building.reparentTo(render)
 
-app = test()
- 
-print(__builtins__.base)
+        self.scene = self.loader.loadModel("models/environment") #charge l'environnement
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
 
+app = test()
 app.run()
