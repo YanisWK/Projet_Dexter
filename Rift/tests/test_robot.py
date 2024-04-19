d72f86cd807c7b271b8045655050b0b18430a43e
vntgogh <ines.ediane29@gmail.com>
2024-04-19 22:58:44 +0200

    tests unitaires

--- Rift/tests/test_robot.py
+++ Rift/tests/test_robot.py
@@ -1,11 +1,14 @@
-from .Dexturret.turret.robot import Robot 
+from Dexturret.simu.robot import Robot 
 from math import cos, sin, radians
+from time import time
 
 import unittest
 
+#tester avec la commande python3 -m tests.test_robot
+
 class TestRobot(unittest.TestCase):
     def test_robot_est_instancce_de_robot(self):
-        test_robot = Robot("Dexter",50,25,0,0)
+        test_robot = Robot(1, 50, 25, 5, 1000/2, 700/2, time())
         self.assertIsInstance(test_robot, Robot)
 
     # def test_robot_avancer(self):
@@ -27,39 +30,24 @@ class TestRobot(unittest.TestCase):
     #    self.assertEqual(test_robot.coordRobot, [(25.0, 0.0), (25.0, 50.0), (-25.0, 50.0), (-25.0, 0.0)])
 
     def test_robot_deplacementRobot(self):  
-        test_robot = Robot("Dexter",50,25,0,0)
+        test_robot = Robot(1, 50, 25, 5, 1000/2, 700/2, time())
         test_robot.deplacementRobot(1)
         self.assertEqual(test_robot.x, 0)
         self.assertEqual(test_robot.y, 0)
 
     def test_robot_coeff_directeur(self):
-        test_robot = Robot("Dexter", 50, 25, 0, 0)
+        test_robot = Robot(1, 50, 25, 5, 1000/2, 700/2, time())
         res = test_robot.coeff_directeur(45) #angle = 45
         res_assert = (cos(radians(45)),sin(radians(45)))
         self.assertEqual(res, res_assert)
 
     def test_robot_detect_distance(self):
-        test_robot = Robot("Dexter", 50, 25, 0, 0)
+        test_robot = Robot(1, 50, 25, 5, 1000/2, 700/2, time())
         simu_longueur = 1000
         simu_largeur = 700
         res = test_robot.detect_distance(simu_longueur, simu_largeur)
         res_assert = round(simu_longueur/2,simu_largeur/2)
         self.assertEqual(res, res_assert)
-
-    def test_robot_set_vitesse_roue(self):
-        test_robot = Robot("Dexter", 50, 25, 0, 0)
-        #test roue gauche
-        test_robot.set_vitesse_roue(1, 10)
-        self.assertEqual(test_robot.vitesse_lineaire_roue_gauche, 10)
-
-        #test roue droite
-        test_robot.set_vitesse_roue(2, 8)
-        self.assertEqual(test_robot.vitesse_lineaire_roue_droite, 8)
-
-        #test pour les deux roues
-        test_robot.set_vitesse_roue(3, 5)
-        self.assertEqual(test_robot.vitesse_lineaire_roue_gauche, 5)
-        self.assertEqual(test_robot.vitesse_lineaire_roue_droite, 5)
-
+        
     if __name__ == '__main__':
         unittest.main()
--- Rift/tests/test_simulation.py
+++ Rift/tests/test_simulation.py
@@ -1,21 +1,24 @@
-from Dexturret.turret.simulation import Simulation
-from Dexturret.turret.robot import Robot
+from Dexturret.simu.simulation import Simulation
+from Dexturret.simu.robot import Robot
+from time import time
 
 import unittest
 
+#tester avec la commande python3 -m tests.test_simulation
+
 class TestSimulation(unittest.TestCase):
     def test_simulation_est_instancce_de_simulation(self):
-        robot = Robot("Dexter", 50, 25, 0, 0)
-        test_simu = Simulation(1, robot, 1000, 700, 60)
+        test_robot = Robot(1, 50, 25, 5, 1000/2, 700/2, time())
+        test_simu = Simulation(1, test_robot, 1000, 700, 60)
         self.assertEqual(test_simu.id, 1)
-        self.assertEqual(test_simu.robot, robot)
-        self.assertEqual(test_simu.longueur, 1000)
-        self.assertEqual(test_simu.largeur, 700)
+        self.assertEqual(test_simu.robot, test_robot)
+        self.assertEqual(test_simu.longueur, 700)
+        self.assertEqual(test_simu.largeur, 1000)
         self.assertEqual(test_simu.fps, 60)
         self.assertTrue(test_simu.awake)
 
     def test_simulation_check_collision_arrete_simu(self):
-        robot = Robot("Dexter", 50, 25, 1100, 0)
+        robot = Robot("Dexter", 50, 25, 5, 1100, 700, time())
         test_simu = Simulation(1, robot, 1000, 700, 60)
         test_simu.check_collision()
         self.assertFalse(test_simu.awake)
