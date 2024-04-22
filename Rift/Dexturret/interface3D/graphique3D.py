9f67e6583962cdfaac3f4d9d7bb7b4eb4022cf22
Victor Roque Gomes <victorloic22@gmail.com>
2024-04-22 11:43:22 +0200

    Ajout d'une plateforme sur panda3D

--- Rift/Dexturret/interface3D/graphique3D.py
+++ Rift/Dexturret/interface3D/graphique3D.py
@@ -2,8 +2,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Geom,GeomTriangles,GeomVertexWriter,GeomVertexFormat,GeomVertexData,TransparencyAttrib
-
+from panda3d.core import Geom,GeomTriangles,GeomVertexWriter,GeomVertexFormat,GeomVertexData,TransparencyAttrib,NodePath,CardMaker,Point3,Vec4
 
 def create_colored_rect(x,z,width,height,colors=None):
     _format=GeomVertexFormat.getV3c4()
@@ -12,7 +11,17 @@ class VueRobot(ShowBase):
     def __init__(self):
         super().__init__()
 
+        # Créer un CardMaker
+        card_maker = CardMaker('card_maker')
 
+        # Définir les dimensions du rectangle
+        card_maker.setFrame(-1, 1, -1, 1)  # left, right, bottom, top
+
+        # Créer le rectangle
+        rectangle_node = NodePath(card_maker.generate())
+
+        # Ajouter le rectangle à la scène
+        rectangle_node.reparentTo(self.render)
             
 app=VueRobot()
 app.run()
