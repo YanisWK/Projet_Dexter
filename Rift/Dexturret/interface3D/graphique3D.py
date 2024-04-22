91e0a8c1004cf165ad8e57f0bdedc5ee0f068497
YanisWK <yanis.saadi03@gmail.com>
2024-04-22 10:49:29 +0200

    Recommencer la partie graphique a 0

--- Rift/Dexturret/interface3D/graphique3D.py
+++ Rift/Dexturret/interface3D/graphique3D.py
@@ -1,30 +1,18 @@
-from panda3d.core import loadPrcFile
-loadPrcFile("config/conf.prc")
-
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
+from panda3d.core import Geom,GeomTriangles,GeomVertexWriter,GeomVertexFormat,GeomVertexData,TransparencyAttrib
+
+
+def create_colored_rect(x,z,width,height,colors=None):
+    _format=GeomVertexFormat.getV3c4()
 
 class VueRobot(ShowBase):
     def __init__(self):
         super().__init__()
-       
 
-        self.robot = Actor("test.glb")
-        self.robot.setScale(0.01, 0.01, 0.01)
-        self.scene.setPos(-8, 58, 0)
-        self.robot.reparentTo(self.render)
 
-        self.building = loader.loadModel("test.glb")
-        self.building.setPos(0,5,0)        
-        self.building.reparentTo(render)
-
-        self.scene = self.loader.loadModel("models/environment") #charge l'environnement
-        self.scene.reparentTo(self.render)
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
-
-app = test()
+            
+app=VueRobot()
 app.run()
