9af367c16d575df106932512b95f4ad082d1490a
Victor Roque Gomes <victorloic22@gmail.com>
2024-06-01 22:40:41 +0200

    Update adaptateurSimu.py

--- Rift/Dexturret/simu/adaptateurSimu.py
+++ Rift/Dexturret/simu/adaptateurSimu.py
@@ -32,7 +32,10 @@ class adaptateurSimu():
             self.robot.vitesse_lineaire_roue_droite = vitesse
 
     def get_position_moteurs(self):
-        """Retourne la position des moteurs au dernier rafraîchissement"""
+        """
+        Retourne : 
+        - la position des moteurs au dernier rafraîchissement
+        """
         return (self.robot.position_moteurs[0], self.robot.position_moteurs[1])
     
     def set_position_moteurs(self, port, offset):
@@ -49,9 +52,28 @@ class adaptateurSimu():
             self.robot.position_moteurs[1] -= offset
 
     def detect_distance(self, simu_longueur, simu_largeur):
+        """
+        Retourn:
+        - la distance entre le robot et un obstacle
+        """
         return self.robot.detect_distance(simu_longueur, simu_largeur)
     
     def calcule_avancer_tourner(self):
+        """la fonction calcule la distance et l'angle parcourus par le robot à chaque rafraîchissement
+        Paramètres :
+        - distance_parcourue : distance parcourue par le robot
+        - angle_parcouru : angle parcouru par le robot
+
+        Variable locale :
+        - angle_RG, angle_RD : position des moteurs au dernier rafraîchissement (en degrés)
+        - dist_RG, dist_RD : distance parcourue par chaque roue (en cm)
+        - distance_parcourue : distance parcourue par le robot (en cm)
+        - angle_parcouru : angle parcouru par le robot (en degrés)
+
+        Retourne :
+        - distance_parcourue : distance parcourue par le robot (en cm)
+        - angle_parcouru : angle parcouru par le robot (en degrés)
+        """
         angle_RG, angle_RD = self.get_position_moteurs()
 
         dist_RG = (abs(angle_RG) / 360) * (2 * self.rayon_des_roues * pi)
@@ -70,24 +92,47 @@ class adaptateurSimu():
 
     @property
     def rayon_des_roues(self):
+        """
+        Retourne :
+        - le rayon des roues du robot (en cm)
+        """
         return self.robot.rayon_des_roues
     
     @property
     def x(self):
+        """Retourne :
+        - la position x du robot (en cm)
+        """
         return self.robot.x
     
     @property
     def y(self):
+        """
+        Retourne :
+        - la position y du robot (en cm)
+        """
         return self.robot.y
     
     @property
     def longueur(self):
+        """
+        Retourne :
+        - la longueur du robot (en cm)
+        """
         return self.robot.longueur
     
     @property
     def largeur(self):
+        """
+        Retourne :
+        - la largeur du robot (en cm)
+        """
         return self.robot.largeur
     
     @property
     def trace(self):
+        """
+        Retourne :
+        - la liste des positions du robot
+        """
         return self.robot.trace
